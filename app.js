94fad08127558e4282a5566ec1d4110b4bcb9cb7
maccana <mikeymccann@gmail.com>
2015-03-03 00:35:00 -0500

    Modified with user CRUD api for cloud mongo db

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require("express"),
+var express = require('express'),
     app = express(),
     formidable = require('formidable'),
     util = require('util')
@@ -8,11 +8,12 @@ var express = require("express"),
 		bodyParser = require('body-parser'),
 		session = require('express-session'),
 		mongoose = require('mongoose'),
+		path = require('path'),
 		cookieParser = require('cookie-parser');
 		
 		
 // Mongoose connection to MongoDB (ted/ted is readonly)
-mongoose.connect('mongodb://ted:ted@ds061797.mongolab.com:61797/theenlighteneddeveloper', 
+mongoose.connect('mongodb://****:****@ds049651.mongolab.com:49651/node-image-upload', 		
 function (error) {
     if (error) {
         console.log(error);
@@ -31,13 +32,21 @@ var User = mongoose.model('users', UserSchema);
 // Serve static files
 // app.use(express.static('./public'));
 // configure app
-app.use(session({secret: 'ssshhhhh'}));
+app.use(session({secret: 'this is a secret'}));
 app.use(cookieParser());
-app.set('views', __dirname + '/views');
+// View config
+app.set('views', path.join( __dirname + '/views'));
 app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, 'bower_components/foundation')));
+// Body parser 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Root URL - sign up form
+app.get('/', function (req, res) {
+    // res.send("<a href='/users'>Show Users</a>");
+		res.render('index.html');
+});
 
 // Routes for mongodb 
 // app.get('/', function (req, res) {
@@ -47,91 +56,42 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/users', function (req, res) {
     User.find({}, function (err, docs) {
 			// render a view here to display the json from mongolab db
-        res.json(docs);
+      //res.json(docs);
+			res.render('users.html', {users: docs} ); 
     });
 });
 
-app.get('/users/:email', function (req, res) {
-    if (req.params.email) {
-        User.find({ email: req.params.email }, function (err, docs) {
-            res.json(docs);
+// Create new user 
+app.post('/users', function(req, res){
+	console.log('User added: ' + JSON.stringify(req.body));
+	var user = new User(req.body);
+  user.save(function(err) {
+    if (err) {
+      return res.send(err);
+    }
+		var data = 'success';
+    res.send(data);
+  });
+	
+});
+
+// Get specific user 
+app.get('/users/:id', function (req, res) {
+    if (req.params.id) {
+        User.findOne({ _id: req.params.id }, function (err, doc) {
+					// render a view here to display the user from mongolab db
+					// var user = docs;
+					//res.json(doc);
+		       res.render('user.html', {user:doc} ); 
         });
     }
 });
-
-
+	
 // index home page
 // app.get('/', function(req, res) {
 //   res.sendFile(__dirname + '/public/index.html');
 // });
 
 
-var sess;
-
-app.get('/',function(req,res){
-	sess=req.session;
-	//Session set when user Request our app via URL
-	if(sess.email)
-	{
-	/*
-	* This line check Session existence.
-	* If exists will do some action.
-	*/
-	res.redirect('/admin');
-	}
-	else{
-	res.render('index.html');
-	}
-});
-
-app.post('/login',function(req,res){
-	sess=req.session;
-	//In this we are assigning email to sess.email variable.
-	//email comes from HTML page.
-	sess.email=req.body.email;
-	res.end('done');
-});
-
-app.get('/admin',function(req,res){
-	sess=req.session;
-	if(sess.email)
-	{
-	res.write('<h1>Hello '+sess.email+'</h1>');
-	res.end('<a href="/logout">Logout</a>');
-	}
-	else
-	{
-	res.write('<h1>Please login first.</h1>');
-	res.end('<a href="+">Login</a>');
-	}
-});
-
-app.get('/logout',function(req,res){
-	req.session.destroy(function(err){
-		if(err){
-		console.log(err);
-		}
-		else
-		{
-		res.redirect('/');
-		}
-	});
-});
-
-// logging out json to the browser via api url
-app.get('/api/:name', function(req, res) {
-  res.json(200, { "hello": req.params.name });
-});
-
-// test route for cookie parser in browser
-app.get('/name/:name', function(req, res) {
-	res.cookie('name', req.params.name)
-	.send('<p>To see cookie, <a href="/name">Go Here!</a></p>')
-});
-app.get('/name', function(req, res) {
-	res.clearCookie('name').send(req.cookies.name);
-});
-
-
-app.listen(8080);
-console.log("Running on port 8080...")
+app.listen(3040);
+console.log("Running on port 3040...")
