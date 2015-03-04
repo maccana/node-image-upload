bbd470ddc405ea08a8a14673f8b97278ecd88d46
maccana <mikeymccann@gmail.com>
2015-03-03 23:33:36 -0500

    Added config directory

---
+++ config/database.js
@@ -0,0 +1,13 @@
+// config/database.js
+module.exports = {
+	
+	'url' : 'mongodb://mike:ispab@ds049651.mongolab.com:49651/node-image-upload'
+
+	// Mongoose connection to MongoDB (ted/ted is readonly)
+	// mongoose.connect('mongodb://mike:ispab@ds049651.mongolab.com:49651/node-image-upload',
+	// function (error) {
+	//     if (error) {
+	//         console.log(error);
+	//     }
+	// });
+};
