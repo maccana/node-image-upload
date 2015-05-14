591da4ae4e00888b4aaae0ac7fdbdaed81fbc928
maccana <mikeymccann@gmail.com>
2015-05-13 23:55:30 -0400

    Clean up file-mover.js

--- tutsplus-passport/services/file-mover.js
+++ tutsplus-passport/services/file-mover.js
@@ -1,18 +1,14 @@
 'use-strict'
 
-var	emptyFolder 	= require('./empty-folder'),
-		thumber 	= require('./thumber'),
-		fs   		= require('fs-extra');
+var	fs  = require('fs-extra');
 
 exports.fileMove = function(temp_path, destPath, filesTransfered, fileCount, callback) {
-
 	console.log("File Mover....");
 	fs.move(temp_path, destPath, function(err) {
 	  	if (err) {
 	    	console.error(err);
 	  	} else {
 			console.log('Completed move for ' + destPath);	
-
 			if(filesTransfered == fileCount) {
 				console.log("Transfers to gallery folder finished...");
 				var success = "Callback received from file mover...";
